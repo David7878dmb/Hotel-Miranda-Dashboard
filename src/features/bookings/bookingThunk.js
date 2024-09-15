b106a98825b12d3d3e2ec8b83d34e01e08f543db
David7878 <david7878.dmb@gmail.com>
2024-09-15 16:06:50 +0200

    Fixed: booking thunk

--- src/features/bookings/bookingThunk.js
+++ src/features/bookings/bookingThunk.js
@@ -2,12 +2,14 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import data from '../../components/json/MOCK_DATA.json';
 
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+const delaytime = 2000;
+
 
 // Thunk para obtener todas las bookings
 export const getAllThunk = createAsyncThunk(
     'booking/getAll',
     async () => {
-        await delay(2000);
+        await delay(delaytime);
         return data;
     }
 );
@@ -16,7 +18,7 @@ export const getAllThunk = createAsyncThunk(
 export const getByIdThunk = createAsyncThunk(
     'booking/getById',
     async (id) => {
-        await delay(2000);
+        await delay(delaytime);
         return data.find(booking => booking.id === id) || null;
     }
 );
@@ -25,7 +27,7 @@ export const getByIdThunk = createAsyncThunk(
 export const createThunk = createAsyncThunk(
     'booking/create',
     async (newBooking) => {
-        await delay(2000);
+        await delay(delaytime);
         alert(`Booking creada: ${JSON.stringify(newBooking)}`);
         return newBooking;
     }
@@ -35,7 +37,7 @@ export const createThunk = createAsyncThunk(
 export const editThunk = createAsyncThunk(
     'booking/edit',
     async ({ id, updatedBooking }) => {
-        await delay(2000); 
+        await delay(delaytime); 
         const oldBooking = data.find(booking => booking.id === id) || null;
         alert(`Booking modificada: ${JSON.stringify(oldBooking)} -> ${JSON.stringify(updatedBooking)}`);
         return updatedBooking;
@@ -46,7 +48,7 @@ export const editThunk = createAsyncThunk(
 export const removeThunk = createAsyncThunk(
     'booking/remove',
     async (id) => {
-        await delay(2000);
+        await delay(delaytime);
         const deletedBooking = data.find(booking => booking.id === id) || null;
         alert(`Booking eliminada: ${deletedBooking}`);
         return id;
